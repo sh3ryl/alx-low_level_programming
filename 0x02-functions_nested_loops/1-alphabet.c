2d85af0e814fae4221e8ff6c6d61ae969cfd371c
sh3ryl <sherrellndunge@gmail.com>
2023-03-26 16:59:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{ _putchar(x);
+	}
+	_putchar('\n');
+}
