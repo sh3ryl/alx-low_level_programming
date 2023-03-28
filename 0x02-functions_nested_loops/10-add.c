210f3d0dc63bb494b107822b902c80b658d1fab3
sh3ryl <sherrellndunge@gmail.com>
2023-03-28 11:23:44 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that finds the sum of integers
+ * @x: integer
+ * @y: integer
+ * Return: sum of integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
