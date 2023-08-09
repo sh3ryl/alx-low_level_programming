7dbf00a0b364b82257643c14e81f77b2ecaae147
sh3ryl <sherrellndunge@gmail.com>
2023-08-09 15:34:29 -0700

    100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,7 +9,10 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int z = 0; int w = 0; int x = 0; int y = 0;
+	int z = 0;
+	int w = 0;
+	int x = 0;
+	int y = 0;
 	char *s;
 
 	if (ac == 0 || av == NULL)
@@ -17,38 +20,33 @@ char *argstostr(int ac, char **av)
 
 	while (w < ac)
 	{
-		while(av[w][x])
+		while (av[w][x])
 		{
 			z++;
 			x++;
 		}
-		
 		x = 0;
 		w++;
 	}
 	s = malloc((sizeof(char) * z) + ac + 1);
-	
+
 	w = 0;
 
 	while (av[w])
 	{
-		while(av[w][x])
+		while (av[w][x])
 		{
 			s[y] = av[w][x];
 			y++;
 			x++;
 		}
-
 		s[y] = '\n';
 
 		x = 0;
 		y++;
 		w++;
 	}
-
 	y++;
 	s[y] = '\0';
 	return (s);
 }
- 
-
